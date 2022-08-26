212a25e33d80b18767a3e3460c5b137ae2cf6155
dthomasson8 <drew.thomasson@technologyadvice.com>
2022-08-25 21:15:41 -0500

    completed question 3 and working through understanding data of question 4

--- scripts/thomasson.sql
+++ scripts/thomasson.sql
@@ -12,5 +12,32 @@ GROUP BY p.height, p.namefirst, p.namelast, p.playerid, a.teamid, a.g_all, a.pla
 ORDER BY p.height;
 ---2. Eddie Gaedel, who was 3'7" (43 inches tall). Eddie played in a total of one game for the St. Louis Browns (SLA).
 
-SELECT 
-p.namefirst, p.namelast, p.playerid, a.teamid, 
+SELECT *
+from schools
+WHERE schoolname LIKE 'Vanderbilt%';
+
+SELECT *
+FROM collegeplaying
+WHERE schoolid LIKE 'vandy';
+
+SELECT *
+FROM salaries;
+
+WITH total_salaries AS (
+SELECT playerid, SUM(salary) AS total_salary
+    FROM salaries
+    GROUP BY playerid)
+    
+SELECT DISTINCT p.playerid, namefirst, namelast, ts.total_salary
+FROM people AS p
+LEFT JOIN collegeplaying as c
+ON p.playerid = c.playerid
+LEFT JOIN total_salaries as ts
+on p.playerid = ts.playerid
+WHERE schoolid LIKE 'vandy' AND ts.total_salary IS NOT NULL
+ORDER BY ts.total_salary DESC;
+---3. David Price (priceda01) made a total salary of $81,851,296.00 in the majors.
+
+SELECT * 
+FROM fielding
+CASE WHEN ;
