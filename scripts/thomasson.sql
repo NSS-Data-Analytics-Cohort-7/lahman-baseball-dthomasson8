a99fd89abf287651d900795ae407aee1850d6ccb
dthomasson8 <drew.thomasson@technologyadvice.com>
2022-08-27 12:40:04 -0500

    Starting on 6. Hannah helped walk through casts for 5.

--- scripts/thomasson.sql
+++ scripts/thomasson.sql
@@ -58,6 +58,25 @@ group by position;
 
 
 ---5. 5. Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places. Do the same for home runs per game. Do you see any trends?
----USE CASE STATEMENT TO GET DECADES.TEAMS IS THE TABLE YOU WANT. USE YOUR BRAIN, YOU ARE NOT DUM DUM.
 SELECT *
 FROM teams;
+---
+SELECT ROUND(CAST(sum(so) AS NUMERIC)/sum(g/2),2) AS avg_strikeouts,
+       ROUND(CAST(sum(hr) AS NUMERIC)/sum(g/2),2) AS avg_homeruns,
+CASE WHEN yearid BETWEEN '1920' AND '1929' THEN '1920s'
+     WHEN yearid BETWEEN '1930' AND '1939' THEN '1930s'
+     WHEN yearid BETWEEN '1940' AND '1949' THEN '1940s'
+     WHEN yearid BETWEEN '1950' AND '1959' THEN '1950s'
+     WHEN yearid BETWEEN '1960' AND '1969' THEN '1960s'
+     WHEN yearid BETWEEN '1970' AND '1979' THEN '1970s'
+     WHEN yearid BETWEEN '1980' AND '1989' THEN '1980s'
+     WHEN yearid BETWEEN '1990' AND '1999' THEN '1990s'
+     WHEN yearid BETWEEN '2000' AND '2009' THEN '2000s'
+     WHEN yearid BETWEEN '2010' AND '2019' THEN '2010s'
+     ELSE 'Other'
+     END AS decades
+FROM teams
+GROUP BY decades
+ORDER BY decades;
+---CAST help from Hannah.
+
