a70daad08fabec0feacdf1a7d304109d1f52c74f
dthomasson8 <drew.thomasson@technologyadvice.com>
2022-08-27 13:59:40 -0500

    stumbing though question 6..i think it can all come from the pattingpost table

--- scripts/thomasson.sql
+++ scripts/thomasson.sql
@@ -79,4 +79,16 @@ FROM teams
 GROUP BY decades
 ORDER BY decades;
 ---CAST help from Hannah.
+---5. ANSWER: Strikeouts increased as the years went on and homeruns were slowly increasing as well. There were some decades that this was not the case, but as an overall trend, both categories increased.
 
+
+---6. Find the player who had the most success stealing bases in 2016, where __success__ is measured as the percentage of stolen base attempts which are successful. (A stolen base attempt results either in a stolen base or being caught stealing.) Consider only players who attempted _at least_ 20 stolen bases.
+SELECT *
+FROM battingpost;
+---stolen + caught /stolen?
+SELECT playerid, sb as stolen_bases, cs as caught_stealing,
+       AS attempts
+FROM battingpost
+WHERE attempts > 19
+GROUP BY playerid, sb, cs
+ORDER BY cs DESC;
